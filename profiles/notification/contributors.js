3c63c4b816cca625d7b007af84842dbd1325b2fe
Viktor Haag <viktor.haag@gmail.com>
2025-09-22 16:28:14 -0400

    Initial checkpoint for notification profile materials
    - fixture examples that sketch out the data model
    
    - fragments for the profile's actions
    
    - boilerplate for caliper entities
    
    - boilerplate for profile errata
    
    - boilerplate for contributors and local bibilo respec files

---
+++ profiles/notification/contributors.js
@@ -0,0 +1,47 @@
+var _contributors = [
+    {
+        name: "Viktor Haag",
+        company: "D2L Corporation",
+        role: "Author"
+    },
+    {
+        name: "Ben Motz",
+        company: "Indiana University",
+        role: "Author"
+    },
+    {
+        name: "Harmony Jankowski",
+        company: "Indiana University",
+        role: "Author"
+    },
+    {
+        name: "Robert Long",
+        company: "Unicon",
+        role: "Author"
+    },
+    {
+        name: "Bart Pursel",
+        company: "Unizin",
+        role: "Author"
+    },
+    {
+        name: "Derek Gleim",
+        company: "Unizin",
+        role: "Author"
+    },
+    {
+        name: "James Russell",
+        company: "Unizin",
+        role: "Author"
+    },
+    {
+        name: "Kyle Unruh",
+        company: "Unizin",
+        role: "Author"
+    },
+    {
+        name: "Jacques Menasche",
+        company: "1EdTech",
+        role: "Editor"
+    }
+]
---
+++ profiles/notification/local-biblio.js
@@ -0,0 +1,28 @@
+var _localBiblioLibrary = {
+    "CALIPER-NOTIFICATION-12": {
+        "title": "Caliper Analytics® Notification Profile 1.2-extension",
+        "href": "https://www.imsglobal.org/spec/caliper-notification/v1p2-extension/",
+        "publisher": "1EdTech Consortium",
+        "authors": [
+            "Gleim, Derek",
+            "Haag, Viktor",
+            "Jangkowski, Harmony",
+            "Long, Robert",
+            "Menasche, Jacques",
+            "Motz, Ben",
+            "Pursel, Bart",
+            "Russell, James",
+            "Unruh, Kyle"
+        ]
+    },
+    "CALIPER-NOTIFICATION-12-CONTEXT": {
+        "title": "Caliper Analytics® Notification Profile Context 1.2 Extension",
+        "href": "https://purl.imsglobal.org/spec/caliper-notification/v1p2-extension/context/",
+        "publisher": "1EdTech Consortium"
+    },
+    "CALIPER-NOTIFICATION-12-ERRATA": {
+        "title": "Caliper Analytics® Notification Profile 1.2 Extension Errata",
+        "href": "https://www.imsglobal.org/spec/caliper-notification/v1p2-extension/errata/",
+        "publisher": "1EdTech Consortium"
+    }
+};
